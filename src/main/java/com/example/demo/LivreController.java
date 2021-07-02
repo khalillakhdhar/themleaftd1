c1588026d444aca9968b4f9de9ea850db6a383de
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-07-02 10:01:02 +0100

    changes

--- src/main/java/com/example/demo/LivreController.java
+++ src/main/java/com/example/demo/LivreController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,11 +10,19 @@ import com.example.demo.classes.Livre;
 
 @Controller
 public class LivreController {
-@RequestMapping(method =RequestMethod.GET,path = "/" )
+@RequestMapping(method =RequestMethod.GET,path = "livre" )
 public String getLivre(Model m)
 {
 	Livre l=new Livre("mon livre", "Moi", 3);
 	m.addAttribute("livre",l.toString());
 	return "livre";
 }
+@GetMapping("")
+public String createLivre()
+{
+return "create";	
+}
+
+
+
 }
