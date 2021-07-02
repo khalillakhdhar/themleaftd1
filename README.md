2fb594b4269bf5109681b8497d0c536ff65c163f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-07-02 09:07:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# themleaftd1
+ 
---
+++ src/main/java/com/example/demo/Themleaftd1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Themleaftd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Themleaftd1Application.class, args);
+	}
+
+}
