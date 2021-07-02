106eaff55ee9a046164518fb4b6f0baf9e15e73e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-07-02 10:03:56 +0100

    Update LivreController.java

--- src/main/java/com/example/demo/LivreController.java
+++ src/main/java/com/example/demo/LivreController.java
@@ -5,15 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.classes.Livre;
 
 @Controller
 public class LivreController {
-@RequestMapping(method =RequestMethod.GET,path = "livre" )
-public String getLivre(Model m)
+@RequestMapping(method =RequestMethod.POST,path = "livre" )
+public String getLivre(Model m,@RequestParam String titre,@RequestParam String auteur,@RequestParam double prix)
 {
-	Livre l=new Livre("mon livre", "Moi", 3);
+	Livre l=new Livre(titre,auteur,prix);
 	m.addAttribute("livre",l.toString());
 	return "livre";
 }
